295f216226f89cf77343d20c04e579e9817dd199
Gonçalo Morais <gnclmorais@gmail.com>
2021-04-17 17:29:53 +0100

    Set Ember Leaflet up
    Simple London map, for now.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,6 +12,15 @@ module.exports = function (defaults) {
         ],
       },
     },
+    fingerprint: {
+      exclude: [
+        'images/layers-2x.png',
+        'images/layers.png',
+        'images/marker-icon-2x.png',
+        'images/marker-icon.png',
+        'images/marker-shadow.png',
+      ],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
